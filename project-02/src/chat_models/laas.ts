a14007daa96853e0a20a5015dcc864b847c10591
jeongsk <jeongseok@wantedlab.com>
2024-08-09 11:25:14 +0900

    refactor(laas.ts): 클래스 및 인터페이스 이름 변경, LLM 클래스를 확장하여 기능 개선 feat(laas.ts): API 요청 및 메시지 처리 로직 추가, 오류 처리 개선

--- project-02/src/chat_models/laas.ts
+++ project-02/src/chat_models/laas.ts
@@ -4,41 +4,112 @@ import {
   BaseChatModel,
   BaseChatModelCallOptions,
   BaseChatModelParams,
-  BindToolsInput
+  BindToolsInput,
 } from "@langchain/core/language_models/chat_models";
+import { LLM } from "@langchain/core/language_models/llms";
 import { AIMessage, BaseMessage } from "@langchain/core/messages";
 import axios from "axios";
-
 
 const LAAS_ROLE_MAP: Record<string, string> = {
   human: "user",
   ai: "assistant",
 };
 
-export interface ChatLaaSCallOptions extends BaseChatModelCallOptions {
+export interface LaaSCallOptions extends BaseChatModelCallOptions {
   params?: Record<string, string>;
-  service_type?: "AZURE" | "OPENAI" | "NCP" | "UPSTAGE" | "GOOGLE" | "AWS" | "ANTHROPIC";
+  service_type?:
+    | "AZURE"
+    | "OPENAI"
+    | "NCP"
+    | "UPSTAGE"
+    | "GOOGLE"
+    | "AWS"
+    | "ANTHROPIC";
   model?: string;
   hash?: string;
   tools?: BindToolsInput[];
 }
 
-export interface ChatLaaSAIFields
-  extends BaseChatModelParams {
+export interface LaaSAIFields extends BaseChatModelParams {
   apiKey?: string;
   projectId?: string;
   hash?: string;
 }
 
-export class ChatLaaS extends BaseChatModel<ChatLaaSCallOptions> {
+export class LaaS extends LLM<LaaSCallOptions> {
   private baseUrl = "https://api-laas.wanted.co.kr/api/preset";
   private apiKey?: string;
   private hash?: string;
   private projectId?: string;
 
-  constructor(
-    fields?: ChatLaaSAIFields
+  constructor(fields?: LaaSAIFields) {
+    super(fields ?? {});
+    this.apiKey = fields?.apiKey;
+    this.hash = fields?.hash;
+    this.projectId = fields?.projectId;
+  }
+
+  async _call(
+    prompt: string,
+    options: this["ParsedCallOptions"],
+    runManager?: CallbackManagerForLLMRun,
+  ): Promise<string> {
+    const response = await this._request(prompt, options);
+    return response?.choices[0]?.message?.content ?? "";
+  }
+
+  _llmType(): string {
+    return "laas";
+  }
+
+  async _request(
+    prompt: string,
+    { signal, ...options }: this["ParsedCallOptions"],
   ) {
+    const body = {
+      hash: this.hash,
+      ...options,
+      ...(prompt && {
+        messages: [
+          {
+            role: "user",
+            content: prompt,
+          },
+        ],
+      }),
+    };
+
+    const headers = {
+      "Content-Type": "application/json",
+      apiKey: this.apiKey,
+      project: this.projectId,
+    };
+
+    try {
+      const response = await axios.post(
+        `${this.baseUrl}/chat/completions`,
+        body,
+        { headers, signal },
+      );
+
+      return response.data;
+    } catch (error) {
+      console.error(
+        "API request failed:",
+        axios.isAxiosError(error) ? error.response?.data : error,
+      );
+      throw error;
+    }
+  }
+}
+
+export class ChatLaaS extends BaseChatModel<LaaSCallOptions> {
+  private baseUrl = "https://api-laas.wanted.co.kr/api/preset";
+  private apiKey?: string;
+  private hash?: string;
+  private projectId?: string;
+
+  constructor(fields?: LaaSAIFields) {
     super(fields ?? {});
     this.apiKey = fields?.apiKey;
     this.hash = fields?.hash;
@@ -49,7 +120,11 @@ export class ChatLaaS extends BaseChatModel<ChatLaaSCallOptions> {
     return "laas";
   }
 
-  async _generate(messages: BaseMessage[], options: this["ParsedCallOptions"], runManager?: CallbackManagerForLLMRun): Promise<ChatResult> {
+  async _generate(
+    messages: BaseMessage[],
+    options: this["ParsedCallOptions"],
+    runManager?: CallbackManagerForLLMRun,
+  ): Promise<ChatResult> {
     const stop = options?.stop;
     const response = await this._request(messages, options, stop, runManager);
     const content = response.choices[0].message.content;
@@ -74,22 +149,27 @@ export class ChatLaaS extends BaseChatModel<ChatLaaSCallOptions> {
     });
 
     return {
-      generations: [{
-        text: content,
-        message: message,
-        generationInfo: response,
-      }],
+      generations: [
+        {
+          text: content,
+          message: message,
+          generationInfo: response,
+        },
+      ],
     };
   }
 
-  async _request(messages: BaseMessage[], {
-    ...options
-  }: this["ParsedCallOptions"], stop: string[] | undefined, runManager?: CallbackManagerForLLMRun) {
+  async _request(
+    messages: BaseMessage[],
+    { signal, ...options }: this["ParsedCallOptions"],
+    stop: string[] | undefined,
+    runManager?: CallbackManagerForLLMRun,
+  ) {
     const body = {
       hash: this.hash,
       ...options,
       messages: messages.map((msg) => ({
-        role:  LAAS_ROLE_MAP[msg._getType()] || msg.lc_kwargs?.role || 'user',
+        role: LAAS_ROLE_MAP[msg._getType()] || msg.lc_kwargs?.role || "user",
         content: msg.content,
       })),
     };
@@ -104,14 +184,16 @@ export class ChatLaaS extends BaseChatModel<ChatLaaSCallOptions> {
       const response = await axios.post(
         `${this.baseUrl}/chat/completions`,
         body,
-        { headers }
+        { headers, signal },
       );
 
       return response.data;
     } catch (error) {
-      console.error("API request failed:", axios.isAxiosError(error) ? error.response?.data : error);
+      console.error(
+        "API request failed:",
+        axios.isAxiosError(error) ? error.response?.data : error,
+      );
       throw error;
     }
   }
-
 }
